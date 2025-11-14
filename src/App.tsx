614d2b8cd1260ecb565c2393fdc01671a9f34a8b
jessicatinguely <jessicatinguely@icloud.com>
2025-11-14 10:02:35 -0300

    :sparkles: Construção da Página de Cadastro e Login do Usuário

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ function App() {
         <Navbar />
         <div className="min-h-[80vh]">
           <Routes>
-            <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/cadastro" element={<Cadastro />} />
