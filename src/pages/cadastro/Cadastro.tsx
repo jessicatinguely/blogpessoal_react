7f10e03d413c5f4371fcb68b2afca3327a6b157a
jessicatinguely <jessicatinguely@icloud.com>
2025-11-14 17:36:10 -0300

    :bug: Correção função retornar

--- src/pages/cadastro/Cadastro.tsx
+++ src/pages/cadastro/Cadastro.tsx
@@ -31,7 +31,7 @@ function Cadastro() {
   }, [usuario]);
 
   function retornar() {
-    navigate('/login');
+    navigate('/');
   }
 
   function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
