e0e0430e02026108ecc2b0accb5f422c2455e9ee
jessicatinguely <jessicatinguely@icloud.com>
2025-11-25 22:27:20 -0300

    :sparkles: Implementação useRef

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: 'https://blogpessoal-jaek.onrender.com'
 });
 
 // Função para cadastrar usuário sem token
